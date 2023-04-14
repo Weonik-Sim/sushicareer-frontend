a9e0d4cbfcd9f5e1be8968cf5840b8cd590eef4b
sarah_sim <sarah_sim@MacBook-Pro.local>
2023-04-15 03:22:58 +0900

    Edit Contract

--- pages/user.tsx
+++ pages/user.tsx
@@ -2,8 +2,47 @@ import Web3 from 'web3';
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import TokenArtifact from "../contracts/AIB.json";
+import { useState } from 'react';
 
 const User: NextPage = () => {
+    const [userName, setUserName] = useState('');
+    const [companyName, setCompanyName] = useState('');
+    const [companyUrl, setCompanyUrl] = useState('');
+
+    const userNameChange = (e) => {
+        setUserName(e.target.value);
+    };
+
+    const companyNameChange = (e) => {
+        setCompanyName(e.target.value);
+    };
+
+    const companyUrlChange = (e) => {
+        setCompanyUrl(e.target.value);
+    };
+
+    const [fromIdInfo, setFromIdInfo] = useState('');
+
+    const fromIdInfoChange = (e) => {
+        setFromIdInfo(e.target.value);
+    };
+
+    const [fromId, setFromId] = useState('');
+    const [toId, setToId] = useState('');
+    const [sushi, setSushi] = useState('');
+
+    const fromIdChange = (e) => {
+        setFromId(e.target.value);
+    };
+
+    const toIdChange = (e) => {
+        setToId(e.target.value);
+    };
+
+    const sushiChange = (e) => {
+        setSushi(e.target.value);
+    };
+
     const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 
     const contractAddress = process.env.NEXT_PUBLIC_THIRDWEB_AUTH_PRIVATE_KEY || "";
@@ -11,20 +50,23 @@ const User: NextPage = () => {
     const contract = new web3.eth.Contract(abi, contractAddress);
 
     async function createUser() {
+        console.log("userName: ", userName);
+        console.log("companyName: ", companyName);
+        console.log("companyUrl: ", companyUrl);
         const accounts = await web3.eth.getAccounts();
         console.log("accounts: ", accounts);
-        const result = await contract.methods._createEmployee("SIM_WEONIK", "SARAH_Inc", "https://sarah30.slack.com").send({ from: accounts[0] });
+        const result = await contract.methods._createEmployee(userName, companyName, companyUrl).send({ from: accounts[0] });
         console.log(result);
     };
 
     async function getEmployeeInfo() {
-        const result = await contract.methods._getEmployeeInfo().call();
+        const result = await contract.methods._getEmployeeInfo(fromIdInfo).call();
         console.log("getEmployeeInfo: ", result);
     };
 
     async function sendSushi() {
         const accounts = await web3.eth.getAccounts();
-        const result = await contract.methods._sendSushi(1, 100).send({ from: accounts[0] });
+        const result = await contract.methods._sendSushi(fromId, toId, sushi).send({ from: accounts[0] });
         console.log("setEmployeeJoinCompany: ", result);
     };
 
@@ -54,16 +96,26 @@ const User: NextPage = () => {
                     </p>
                 </div>
                 <div>
+                    <input type="text" name="user_name" value={userName} onChange={userNameChange} placeholder="Your Name" />
+                    <input type="text" name="company_name" value={companyName} onChange={companyNameChange} placeholder="Your Company Name" />
+                    <input type="text" name="company_url" value={companyUrl} onChange={companyUrlChange} placeholder="Your Company Url" />
                     <button onClick={createUser}>Register</button>
                 </div>
+                <br />
 
                 <div>
+                    <input type="text" name="from_id_info" value={fromIdInfo} onChange={fromIdInfoChange} placeholder="Your Id" />
                     <button onClick={getEmployeeInfo}>get User Info</button>
                 </div>
+                <br />
 
                 <div>
+                    <input type="text" name="from_id" value={fromId} onChange={fromIdChange} placeholder="Your Id" />
+                    <input type="text" name="to_id" value={toId} onChange={toIdChange} placeholder="Send Id" />
+                    <input type="text" name="sushi" value={sushi} onChange={sushiChange} placeholder="Sushi" />
                     <button onClick={sendSushi}>Send Sushi</button>
                 </div>
+                <br />
 
                 {/* <div>
                     <button onClick={setEmployeeLeaveCompany}>Leave Company</button>
